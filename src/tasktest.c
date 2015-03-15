3d765c7ccf7e8808e6cd08797ce83b4d631ca552
colin8930 <colin8930@gmail.com>
2015-03-15 11:31:11 +0800

    random create and delete tasks to test the task creating

--- src/tasktest.c
+++ src/tasktest.c
@@ -14,8 +14,8 @@ typedef struct A_BLOCK_LINK
 extern xBlockLink xStartSize;
 int prng(void) __attribute__((naked));
 xTaskHandle xHandle = NULL;
-
-
+extern xTaskHandle xTaskGetIdleTaskHandle( void );
+extern xTaskHandle xcmdHandle;
 
 
 void test_task(void *pvParameters)
@@ -23,20 +23,22 @@ void test_task(void *pvParameters)
     while(1);	
 }
 void tasktest_command(int n, char *argv[]){
-
+	xTaskHandle xIHandle;
+	xIHandle = xTaskGetIdleTaskHandle();
+	
     int handle;
     handle = host_action(SYS_SYSTEM, "mkdir -p output");
     handle = host_action(SYS_SYSTEM, "touch output/syslog.txt");
-
     handle = host_action(SYS_OPEN, "output/syslog.txt", 7);
-
-    while (1) {
+    while ( 1 ) {
+        vTaskPrioritySet(xIHandle, 0);
         unsigned short stacksize = prng() & 0xFF; //0~255
         fio_printf(1, "try to create task %d bytes\r\n", stacksize);
         if(xTaskCreate(test_task,
                 (signed portCHAR *) "test",
-                stacksize /* stack size */, NULL, tskIDLE_PRIORITY + 1, NULL) == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) break;
-                fio_printf(1, "created successfully\r\n");
+                stacksize /* stack size */, NULL, tskIDLE_PRIORITY + 1, &xHandle) == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) break;
+ 
+        fio_printf(1, "created successfully\r\n");
 
         
         signed char buf[1024];
@@ -50,6 +52,7 @@ void tasktest_command(int n, char *argv[]){
 
 
         xBlockLink *pxBlock;
+        
         vTaskSuspendAll();
         {
 
@@ -61,7 +64,7 @@ void tasktest_command(int n, char *argv[]){
                 char tmp[10];
                 sprintf( tmp, ( char * ) "\t%d\t", out );
                 strcpy(str, tmp);
-                fio_printf(1, str);
+                fio_printf(1, tmp);
                 host_action(SYS_WRITE, handle, (void *)tmp, strlen(tmp));
 
                 pxBlock = pxBlock->pxNextSizeBlock;
@@ -69,11 +72,22 @@ void tasktest_command(int n, char *argv[]){
             }
 		
 	    }
-	    xTaskResumeAll();
-    }
-    
-    host_action(SYS_CLOSE, handle);
+	    
+	   
     xTaskResumeAll();
+	    
+	    if( stacksize % 3 == 1 ||stacksize % 3 == 2){
+			char tmp[] ="\n\r this task is deleted \n\r";
+			host_action(SYS_WRITE, handle, (void *)tmp, strlen(tmp));
+            vTaskDelete( xHandle );
+            vTaskPrioritySet( xIHandle, 2 );
+            vTaskDelay( 3 );
+        }
+    }
+
+    host_action(SYS_CLOSE, handle);
+    
 }
     
+
 
